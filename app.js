7379012be89f9e226803bb8e7f0a3b3542fb797c
jef1993 <jeffrey24493@gmail.com>
2021-08-13 03:06:17 +0100

    day 2 end

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const btn = document.querySelectorAll(".btns__btn");
 const controlsBtn = document.querySelectorAll(".controls__btn");
+const btns = document.querySelector(".btns");
+const btn = document.querySelectorAll(".btns__btn");
+const btnType = document.querySelectorAll(".btns__type");
+const btnKey = document.querySelectorAll(".btns__key");
 
 const playAudio = (url) => {
   const audio = document.createElement("audio");
@@ -14,41 +17,71 @@ const playAudio = (url) => {
 
 let currentFile = 1;
 let layout = [
-  ["perc", 1],
-  ["crash", 1],
-  ["tom-high", 1],
-  ["ride", 1],
-  ["hihat-open", 1],
-  ["snare", 1],
-  ["tom-mid", 1],
-  ["clap", 1],
-  ["hihat-closed", 1],
-  ["snare", 1],
-  ["tom-low", 1],
-  ["kick", 1],
+  ["perc", 1, 15, "q"],
+  ["clap", 1, 12, "w"],
+  ["crash", 1, 11, "e"],
+  ["ride", 1, 3, "r"],
+  ["hihat-open", 1, 10, "a"],
+  ["clap", 1, 12, "s"],
+  ["tom-high", 1, 2, "d"],
+  ["tom-mid", 1, 2, "f"],
+  ["hihat-closed", 1, 15, "z"],
+  ["snare", 1, 10, "x"],
+  ["kick", 1, 15, "c"],
+  ["tom-low", 1, 2, "v"],
 ];
 
 const playAudioTarget = function (i) {
-  playAudio(`audio/${layout[i][0]}/${layout[i][0]}_0${layout[i][1]}.wav`);
+  const audioNumber = layout[i][1].toString().padStart(2, "0");
+  playAudio(`audio/${layout[i][0]}/${layout[i][0]}_${audioNumber}.wav`);
 };
 
+// Detect instrument selection
 controlsBtn.forEach((el, index) => {
+  el.value = 1;
+  el.min = 1;
+  el.max = layout[index][2];
   el.addEventListener("change", (e) => {
-    console.log(e.target.value);
     layout[index][1] = e.target.value;
     playAudioTarget(index);
   });
 });
 
-btn.forEach((el, index) =>
+// Play selected audio on click
+btn.forEach((el, index) => {
   el.addEventListener("click", () => {
     playAudioTarget(index);
-  })
-);
-
-document
-  .querySelector(`.controls__btn--1`)
-  .addEventListener("click", function () {
-    document.querySelector(`.controls__btn--1`).innerHTML = currentFile;
-    currentFile = currentFile < 3 ? currentFile + 1 : 1;
   });
+
+  //Prevent double click selecting text
+  el.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+  });
+});
+// Label instruments' type on button
+btnType.forEach((el, index) => {
+  el.innerHTML = layout[index][0];
+});
+
+// Label input key on button
+btnKey.forEach((el, index) => {
+  el.innerHTML = layout[index][3];
+});
+
+// Play selected audio on keydown
+
+document.addEventListener("keydown", (e) => {
+  const targetIndex = layout.findIndex((el) => el[3] === e.key);
+
+  if (targetIndex !== -1) {
+    playAudioTarget(targetIndex);
+    btn[targetIndex].classList.add("keyPress");
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  const targetIndex = layout.findIndex((el) => el[3] === e.key);
+  if (targetIndex !== -1) {
+    btn[targetIndex].classList.remove("keyPress");
+  }
+});
